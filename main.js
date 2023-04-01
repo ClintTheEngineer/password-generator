2961887890df2f52aab04c1eaaf804f077f749d9
ClintTheEngineer <81938100+ClintTheEngineer@users.noreply.github.com>
2023-04-01 00:28:56 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ const prompt = require('prompt-sync')();
 
 const passwordGenerator =(minNum = 12, maxNum = 20)=> {
 let totalCharacterList;
+let passwordLength;
+
+const randomLength =()=>{
+   return Math.floor(Math.random() * (maxNum - minNum + 1)+ minNum) 
+}
 const numbers = [];
     for(let i = 0; i < 10; i++){
         numbers.push(i)
@@ -18,8 +23,14 @@ const uppercaseAlphabetCharacters = [];
       alphabetCharacters.forEach(e =>{
         uppercaseAlphabetCharacters.push(e.toUpperCase())
     })
-
-let passwordLength = prompt(`Choose your desired password length. Enter a number between ${minNum}-${maxNum}: `);
+let passwordChoice = prompt(`Would you like us to generate a password of random length? Enter Yes or No: `);
+if (passwordChoice === 'Yes' || passwordChoice === 'yes' || passwordChoice === 'Y' || passwordChoice === 'y'){
+    passwordLength = randomLength();
+} else if (passwordChoice === 'No' || passwordChoice === 'no' || passwordChoice === 'N' || passwordChoice === 'n'){
+    passwordLength = prompt(`Choose your desired password length. Enter a number between ${minNum}-${maxNum}: `);
+} else {
+    throw new Error('Please select Yes or No for random password length')
+}
 if (!isNaN(passwordLength)) passwordLength = Number(passwordLength);
 if(typeof passwordLength !== 'number') throw new Error('Invalid input type. Please enter a number.');
 if(passwordLength > maxNum || passwordLength < minNum) throw new Error(`Please enter a number between ${minNum}-${maxNum}.`)
